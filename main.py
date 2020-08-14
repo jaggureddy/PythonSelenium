9984219aefcf129c7f05c5e5077c8f98fb02201d
JagguReddy <Jagadeesh6010@gmail.com>
2020-08-13 21:42:04 -0400

    Se-1

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+
+driver = webdriver.Chrome(executable_path='C:\\Users\\JagguMiniPC\\PycharmProjects\\PythonSelenium\\Executables\\chromedriver.exe')
+# driver = webdriver.Firefox(executable_path='C:\\Users\\JagguMiniPC\\PycharmProjects\\PythonSelenium\\Executables\\geckodriver.exe')
+
+
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+driver.implicitly_wait(5000)
+print(driver.title)
+driver.current_url
+driver.refresh()
+# driver.back()
+driver.quit()
+
+
